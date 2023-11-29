ac0aa43e86b1f43478200138156d7b3e5699d06e
imran <zunaeede@gmail.com>
2023-11-29 17:10:50 +0600

    practice - another practice of blog in php

---
+++ 2.2_practice.php
@@ -0,0 +1,20 @@
+<?php
+include '2_practice.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="2.2_practice.php" method="get">
+    <input type="text" name="name" placeholder="Write the name">
+    <textarea name="blog"cols="30" rows="10" placeholder="write the blog"></textarea>
+    <button type="submit" name="submit">Submit</button>
+  </form>
+</body>
+</html>
