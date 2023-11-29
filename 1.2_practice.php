9f0870f8f08b250e70e0474b26952a877544c148
imran <zunaeede@gmail.com>
2023-11-29 10:28:24 +0600

    practice - practice by my self - blog project

---
+++ 1.2_practice.php
@@ -0,0 +1,18 @@
+<?php
+include '1_practice.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="1.2_practice.php" method="get">
+    
+  </form>
+</body>
+</html>
