d29a77768c5681d249763260f35d44c9e75a39de
imran <zunaeede@gmail.com>
2023-11-29 22:03:55 +0600

    practice - not done yet

--- 2_practice.php
+++ 2_practice.php
@@ -1,6 +1,16 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', '', 'pocketcode-test')
+$conn = new mysqli('localhost', 'root', '', 'pocketcode-test');
 
+if($conn){
+  if(isset($_REQUEST['submit'])){
+    $name = $_REQUEST['name'];
+    $blog = $_REQUEST['blog'];
 
-?>
+    $sql = "INSERT into `practice2` (name, blog) values ('$name', '$blog')";
+
+    mysqli_query($conn, $sql);
+    header('location:2.2_practice.php');
+  }
+}
+
