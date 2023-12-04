426399f5409f07d43df59bfc79ff18a5ff2f3496
imran <zunaeede@gmail.com>
2023-12-04 12:50:48 +0600

    practice - making display page

---
+++ 3.2_practice.php
@@ -0,0 +1,28 @@
+<?php
+// Display page
+ include '3_practice.php';
+
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <table >
+
+    <thead>
+    <th></th>
+    <th></th>
+    <th></th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</body>
+</html>
