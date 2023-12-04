ea16b8ac33d07d44c9692339bcd6bb110d91ec13
imran <zunaeede@gmail.com>
2023-12-04 11:57:31 +0600

    practice - fix the comment area.

--- 2.2_practice.php
+++ 2.2_practice.php
@@ -13,8 +13,8 @@ include '2_practice.php';
 <body>
   <form action="2.2_practice.php" method="get">
     <input type="text" name="name1" placeholder="Write the name"><br>
-    <textarea name="blog"cols="30" rows="10" placeholder="write the blog"></textarea><br> // i was forgot to name name1 
-    <button type="submit" name="submit">submit</button>    // first i got forgot to set name in button
+    <textarea name="blog"cols="30" rows="10" placeholder="write the blog"></textarea><br> <!-- i was forgot to name name1 -->
+    <button type="submit" name="submit">submit</button>    <!-- first i got forgot to set name in button -->
   </form>
 </body>
 </html>
